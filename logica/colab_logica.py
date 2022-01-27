5c10e8676a7f22d5910db9c742578a52fc2a7cc9
Greeengooo <aanoshyn@gmail.com>
2022-01-27 18:26:04 +0200

    snowflake extension for colab

--- logica/colab_logica.py
+++ logica/colab_logica.py
@@ -161,6 +161,11 @@ def RunSQL(sql, engine, connection=None, is_final=False):
         else:
             pass
         return None
+    elif engine == "snowflake":
+        if is_final:
+            return pandas.read_sql(sql, connection)
+        else:
+            return connection.execute(sql)
     else:
         raise Exception(
             "Logica only supports BigQuery, PostgreSQL and SQLite " "for now."
@@ -190,6 +195,20 @@ class PostgresRunner(object):
 
     def __call__(self, sql, engine, is_final):
         return RunSQL(sql, engine, self.connection, is_final)
+
+class SnowflakeRunner(object):
+  def __init__(self):
+    global DB_CONNECTION
+    global DB_ENGINE
+    if DB_CONNECTION:
+      self.engine = DB_ENGINE
+      self.connection = DB_CONNECTION
+
+      DB_ENGINE = self.engine
+      DB_CONNECTION = self.connection
+
+  def __call__(self, sql, engine, is_final):
+    return RunSQL(sql, engine, self.connection, is_final)
 
 
 def Logica(line, cell, run_query):
@@ -253,7 +272,9 @@ def Logica(line, cell, run_query):
                     )
 
     with bar.output_to(logs_idx):
-        if engine == "sqlite":
+        if engine == 'snowflake':
+            sql_runner = SnowflakeRunner()
+        elif engine == "sqlite":
             sql_runner = SqliteRunner()
         elif engine == "psql":
             sql_runner = PostgresRunner()
