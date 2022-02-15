cf3658abff140783946b55c3b772c227d9e6d0b6
Vadym Khorkov <vadrpy@gmail.com>
2022-02-15 15:34:39 +0200

    new functions

---
+++ build/lib/logica/compiler/dialect_libraries/bq_library.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+library = """
+->(left:, right:) = {arg: left, value: right};
+
+# All ORDER BY arguments are wrapped, to avoid confusion with
+# column index.
+ArgMin(a) = SqlExpr("ARRAY_AGG({arg} order by [{value}][offset(0)] limit 1)[OFFSET(0)]",
+                    {arg: a.arg, value: a.value});
+
+ArgMax(a) = SqlExpr(
+  "ARRAY_AGG({arg} order by  [{value}][offset(0)] desc limit 1)[OFFSET(0)]",
+  {arg: a.arg, value: a.value});
+
+ArgMaxK(a, l) = SqlExpr(
+  "ARRAY_AGG({arg} order by  [{value}][offset(0)] desc limit {lim})",
+  {arg: a.arg, value: a.value, lim: l});
+
+ArgMinK(a, l) = SqlExpr(
+  "ARRAY_AGG({arg} order by  [{value}][offset(0)] limit {lim})",
+  {arg: a.arg, value: a.value, lim: l});
+
+Array(a) = SqlExpr(
+  "ARRAY_AGG({value} order by [{arg}][offset(0)])",
+  {arg: a.arg, value: a.value});
+
+"""
---
+++ build/lib/logica/compiler/dialect_libraries/snowflake_library.py
@@ -0,0 +1,35 @@
+library = """
+->(left:, right:) = {arg: left, value: right};
+
+ArgMin(a) = SqlExpr(
+  "ARRAY_AGG({arg}) WITHIN GROUP (order by {value})[1]",
+  {arg: a.arg, value: a.value});
+
+ArgMax(a) = SqlExpr(
+  "ARRAY_AGG({arg}) WITHIN GROUP (order by {value} desc)[1]",
+  {arg: a.arg, value: a.value});
+
+ArgMaxK(a, l) = SqlExpr(
+  "ARRAY_SLICE(ARRAY_AGG({arg}) WITHIN GROUP (order by {value} desc), 1, {lim})",
+  {arg: a.arg, value: a.value, lim: l});
+
+ArgMinK(a, l) = SqlExpr(
+  "ARRAY_SLICE(ARRAY_AGG({arg}) WITHIN GROUP (order by {value}), 1, {lim})",
+  {arg: a.arg, value: a.value, lim: l});
+
+Array(a) = SqlExpr(
+  "ARRAY_AGG({value}) WITHIN GROUP (order by {arg})",
+  {arg: a.arg, value: a.value});
+
+RMatch(s, p) = SqlExpr(
+  "REGEXP_LIKE({s}, {p})",
+  {s: s, p: p});
+  
+RExtract(s, p, g) = SqlExpr(
+  "REGEXP_SUBSTR({s}, {p}, {g})",
+  {s: s, p: p, g: g});
+
+ArrayJoin(array, delimiter) = SqlExpr(
+  "ARRAY_TO_STRING({array}, {delimiter})", 
+  {array:, delimiter:});
+"""
