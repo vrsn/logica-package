52c1dd59c41da61c40815dc6b77777b5371a15ae
Greeengooo <oanoshyn@zscaler.com>
2022-10-12 13:27:40 +0200

    fix

--- logica/compiler/dialects.py
+++ logica/compiler/dialects.py
@@ -329,8 +329,8 @@ class Snowflake(Dialect):
             'ToFloat64': 'CAST(%s AS DOUBLE)',
             'AnyValue': 'ANY_VALUE(%s)',
             'ArrayConcat': 'ARRAY_CAT({0}, {1})',
-            'JsonExtractScalar': '{0}:{1}',
             'JsonExtract': '{1}',
+            'JsonExtractScalar': '{1}',
             'Length': 'ARRAY_SIZE(%s)',
             'DateDiff': 'DATEDIFF({0}, {1}, {2})'
         }
@@ -349,7 +349,7 @@ class Snowflake(Dialect):
         return snowflake_library.library
 
     def UnnestPhrase(self):
-        return 'LATERAL FLATTEN(INPUT => {0}) AS pushkin(seq,key,path,index,{1})'
+        return 'LATERAL FLATTEN(INPUT => {0}) AS pushkin_{1}(seq,key,path,index,{1})'
 
     def ArrayPhrase(self):
         return 'ARRAY_CONSTRUCT(%s)'
