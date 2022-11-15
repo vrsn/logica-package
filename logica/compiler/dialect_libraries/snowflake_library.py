43070c9b4879975c82d2dee75ca5aede502e42f5
Vadym Khorkov <vadrpy@gmail.com>
2022-11-15 11:20:56 +0100

    add new func to snowflake lib

--- logica/compiler/dialect_libraries/snowflake_library.py
+++ logica/compiler/dialect_libraries/snowflake_library.py
@@ -98,4 +98,7 @@ From_Unixtime(int) = SqlExpr(
 
 JsonExtractAsString(json, path) = SqlExpr(
   "JSON_EXTRACT_PATH_TEXT({json}, {path})", {json:, path:});
+
+IsNull(expr) = SqlExpr(
+  "IS_NULL_VALUE(json_extract({expr}))", {expr:});
 """
