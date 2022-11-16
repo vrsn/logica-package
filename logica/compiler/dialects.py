0b630c30f3f6c5a6783774173af0e8d6f9e3966c
Greeengooo <oanoshyn@zscaler.com>
2022-11-16 11:57:36 +0100

    fix is_null_value function

--- logica/compiler/dialects.py
+++ logica/compiler/dialects.py
@@ -336,7 +336,7 @@ class Snowflake(Dialect):
             'JsonExtractScalar': 'GET_PATH({0}, {1})',
             'Length': 'ARRAY_SIZE(%s)',
             'DateDiff': 'DATEDIFF({0}, {1}, {2})',
-            'IsNull': '({0} IS NULL OR IS_NULL_VALUE({0}))'
+            'IsNull': '({0} IS NULL OR IS_NULL_VALUE(PARSE_JSON({0})))'
         }
 
     def InfixOperators(self):
